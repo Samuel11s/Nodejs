6faff1ba149adc9e1b5051c1bd68ba87deb6c0b5
Samuel BITINGINGWA <samuelbitingingwa@gmail.com>
2025-03-21 08:45:00 +0200

    First test in command prompt

---
+++ first_test_command_line.js
@@ -0,0 +1,6 @@
+// Just displaying "Hello World" in command prompt (or in Terminal)
+
+console.log("Hello World");
+
+// Open a new terminal and write: /PathToProject>node first_test_command_line.js
+// Return : Hello World
