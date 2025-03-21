47fde88123f49adede08f34af0f458a3cf0ddf4b
Samuel BITINGINGWA <samuelbitingingwa@gmail.com>
2025-03-21 10:15:29 +0200

    demo file location

--- server/demo_file_server.js
+++ server/demo_file_server.js
@@ -3,7 +3,7 @@ var http = require('http'); // Module for the server
 var fs = require('fs'); // Module for making actions to a file, I'm gonna use his function "readFile" to read the content of my html file
 
 http.createServer(function (req, res) {
-  fs.readFile('demofile1.html', function(err, data) {
+  fs.readFile('public/demo.html', function(err, data) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
     res.end();
