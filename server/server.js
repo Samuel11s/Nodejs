c10cc2f2ed87071e9a703da8508f54c6c5bea743
Samuel BITINGINGWA <samuelbitingingwa@gmail.com>
2025-04-07 00:27:54 +0200

    File Updates for Better Overview of the server.js file and demo.html

--- server/server.js
+++ server/server.js
@@ -1,14 +1,25 @@
-// Display: All things I've learned
-
-var http = require('http');
-var myfirstmodule = require('./modules/first_module.js');
-var fs = require('fs')
+const http = require('http');
+const fs = require('fs');
+const myfirstmodule = require('./modules/first_module.js');
 
 http.createServer(function (req, res) {
-  fs.readFile("demo.html", function(err, data){
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    res.write('Check your time here : ' + myfirstmodule.myDateTime());
-    res.end("Report errors while reading the demo file : " + err);
-  })
-}).listen(8080); 
+  if (req.url === '/') {
+    fs.readFile('server/public/demo.html', function(err, data) {
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      if (err) {
+        res.end("Error reading HTML file: " + err.message);
+      } else {
+        res.write(data);
+        res.end();
+      }
+    });
+  } else if (req.url === '/date') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Current date and time: ' + myfirstmodule.myDateTime());
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 Not Found');
+  }
+}).listen(8000, () => {
+  console.log('Server is running on http://localhost:8000');
+});
