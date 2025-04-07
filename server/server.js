d2b63aa0814dd4293ad1f4c864daeb82a0da2a5e
Samuel BITINGINGWA <samuelbitingingwa@gmail.com>
2025-04-07 11:31:09 +0200

    Integrated /path route in server.js.

--- server/server.js
+++ server/server.js
@@ -1,11 +1,12 @@
 const http = require('http');
 const fs = require('fs');
 const myfirstmodule = require('./modules/first_module.js');
+const pathDemo = require('./modules/pathDemo.js');
 
 http.createServer(function (req, res) {
   if (req.url === '/') {
     fs.readFile('public/demo.html', function(err, data) {
-      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
       if (err) {
         res.end("Error reading HTML file: " + err.message);
       } else {
@@ -14,8 +15,17 @@ http.createServer(function (req, res) {
       }
     });
   } else if (req.url === '/date') {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.writeHead(200, { 'Content-Type': 'text/plain; charset=UTF-8' });
     res.end('Current date and time: ' + myfirstmodule.myDateTime());
+  } else if (req.url === '/path') {
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=UTF-8' });
+    res.write('<h1>Démo du module path</h1>');
+    res.write('<ul>');
+      for (const [key, value] of Object.entries(pathDemo)) {
+        res.write(`<li><strong>${key}</strong> : ${value}</li>`);
+      }
+    res.write('</ul>');
+    res.end();
   } else {
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('404 Not Found');
